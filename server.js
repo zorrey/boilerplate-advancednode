5240dddd92ef3f084a60679280683b450623ea88
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-07-21 23:21:43 +0300

    app.set - engine

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
