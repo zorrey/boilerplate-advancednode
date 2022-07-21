85c4cb1f080f4770eaafb60eb678fa9544ff439d
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-07-21 23:24:08 +0300

    view/pug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
